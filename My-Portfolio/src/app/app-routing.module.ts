83b2dae3d4478c9d0ee23305f57833b7a33d9c28
an.izzo <an.izzo@almaviva.it>
2024-07-20 18:23:50 +0200

    added home page

--- My-Portfolio/src/app/app-routing.module.ts
+++ My-Portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes , ExtraOptions} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    loadChildren: () => import('./home/home.module').then(h => h.HomeModule)
+  },
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    redirectTo:'/home'
+  }
+];
+
+// const routerOption : ExtraOptions = {
+//   scrollPositionRestoration:'enabled',
+//   anchorScrolling:'enabled'
+// }
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ My-Portfolio/src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  @ViewChild('skillsDiv') skillsDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('projectsDiv') projectDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('infoDiv') infoDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('contactsDiv') contactsDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('navbar') footButton : ElementRef<HTMLElement> | undefined;
+
+  myNumber : string = 'aizzo450@gmail.com'
+
+  constructor(private el : ElementRef){}
+
+  scrollToSection(id: string) {
+    let element : HTMLElement | null = null;
+
+    if (id === 'skills' && this.skillsDiv) {
+      element = this.skillsDiv.nativeElement
+    }else if (id === 'projects' && this.projectDiv) {
+      element = this.projectDiv.nativeElement
+    }else if (id === 'info' && this.infoDiv) {
+      element = this.infoDiv.nativeElement
+    }else if (id === 'contacts' && this.contactsDiv) {
+      element = this.contactsDiv.nativeElement
+    }else if (id === 'navbar' && this.footButton) {
+      element = this.footButton.nativeElement
+    }
+
+    if (element) {
+      element.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
+}
