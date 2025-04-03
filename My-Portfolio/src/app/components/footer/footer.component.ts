daa25505d06c6e89d754ba86ba0c99647fb447da
andrewizzo <aizzo450@gmail.com>
2025-04-03 17:18:30 +0200

    fix:fixed components

--- My-Portfolio/src/app/components/footer/footer.component.ts
+++ My-Portfolio/src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  @ViewChild('skillsDiv') skillsDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('projectsDiv') projectDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('infoDiv') infoDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('contactsDiv') contactsDiv : ElementRef<HTMLElement> | undefined;
+  @ViewChild('navbar') footButton : ElementRef<HTMLElement> | undefined;
 
+  scrollToSection(id: string) {
+    let element : HTMLElement | null = null;
+
+    if (id === 'skills' && this.skillsDiv) {
+      element = this.skillsDiv.nativeElement
+    }else if (id === 'projects' && this.projectDiv) {
+      element = this.projectDiv.nativeElement
+    }else if (id === 'info' && this.infoDiv) {
+      element = this.infoDiv.nativeElement
+    }else if (id === 'contacts' && this.contactsDiv) {
+      element = this.contactsDiv.nativeElement
+    }else if (id === 'navbar' && this.footButton) {
+      element = this.footButton.nativeElement
+    }
+
+    if (element) {
+      element.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
 }
